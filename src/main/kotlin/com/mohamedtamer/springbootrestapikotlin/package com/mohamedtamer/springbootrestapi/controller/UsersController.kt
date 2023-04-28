502426324a595ca5013d2e1231e46574f3c13f0f
mohamedtamer0 <mt4452242@gmail.com>
2023-04-28 19:36:48 +0300

    Path Parameters

--- src/main/kotlin/com/mohamedtamer/springbootrestapikotlin/package com/mohamedtamer/springbootrestapi/controller/UsersController.kt
+++ src/main/kotlin/com/mohamedtamer/springbootrestapikotlin/package com/mohamedtamer/springbootrestapi/controller/UsersController.kt
@@ -1,8 +1,10 @@
 package com.mohamedtamer.springbootrestapikotlin.`package com`.mohamedtamer.springbootrestapi.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 class UsersController {
@@ -14,5 +16,12 @@ class UsersController {
     @PostMapping("/userpost")
     fun helloPost(): String {
         return "Hello(Post)!"
+    }
+
+
+    // Path Parameters
+    @GetMapping("/users/{id}")
+    fun helloPath(@PathVariable("id") uuid: String): String? {
+        return "Hello!$uuid"
     }
 }
